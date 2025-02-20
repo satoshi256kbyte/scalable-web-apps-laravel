f50391c64fc2e0bcebaf3314733805102dd60890
satoshi.kaneyasu <satoshi.kaneyasu@serverworks.co.jp>
2025-02-20 00:18:47 +0000

    test

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -21,35 +21,26 @@ abstract class Controller
         // $expected_alb_arn = 'arn:aws:elasticloadbalancing:region-code:account-id:loadbalancer/app/load-balancer-name/load-balancer-id';
 
         // Assuming the JWT is in the 'x-amzn-oidc-data' header (you will need to extract it from the request headers)
-        var_dump($request->headers->all());
-        echo "<br>";
         $encoded_jwt = $request->header('x-amzn-oidc-data');
-        echo $encoded_jwt;
-        echo "<br>";
         if ($encoded_jwt === null) {
             return 'No JWT found in headers';
         }
 
         // Decode the JWT header (first part of the JWT)
         $jwt_headers = explode('.', $encoded_jwt);
-        var_dump($jwt_headers);
-        echo "<br>";
         $jwt_head = base64_decode($jwt_headers[0]);
         $decoded_json = json_decode($jwt_head);
-        var_dump($decoded_json);
-        echo "<br>";
-        // $kid = $decoded_json->kid;
-        // echo $kid;
+        $kid = $decoded_json->kid;
 
-        // $region = 'ap-northeast-1'; // AWS リージョン
-        // $url = "https://public-keys.auth.elb.$region.amazonaws.com/$kid";
+        $region = 'ap-northeast-1'; // AWS リージョン
+        $url = "https://public-keys.auth.elb.$region.amazonaws.com/$kid";
 
-        // $client = new Client();
-        // $response = $client->request('GET', $url);
-        // $pub_key = $response->getBody()->getContents();
+        $client = new Client();
+        $response = $client->request('GET', $url);
+        $pub_key = $response->getBody()->getContents();
 
-        // $decoded_payload = JWT::decode($encoded_jwt, $pub_key, ['ES256']);
+        $decoded_payload = JWT::decode($encoded_jwt, $pub_key, ['ES256']);
 
-        // return json_encode($decoded_payload, JSON_PRETTY_PRINT);
+        return json_encode($decoded_payload, JSON_PRETTY_PRINT);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,8 @@ use App\Http\Controllers\GuestController;
 // TOPページ
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
-// 会員用TOPページ
-Route::get('/member', [MemberController::class, 'index'])->name('member');
+// // 会員用TOPページ
+// Route::get('/member', [MemberController::class, 'index'])->name('member');
 
-// ゲスト用TOPページ
-Route::get('/guest', [GuestController::class, 'index'])->name('guest');
+// // ゲスト用TOPページ
+// Route::get('/guest', [GuestController::class, 'index'])->name('guest');
